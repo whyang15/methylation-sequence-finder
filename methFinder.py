b92e25bf17bcf0aa906ae6b4a8376a3e4daf62f3
whyang15 <wy15@mac.com>
2024-08-12 14:23:48 -0700

    Bug fixes and add unit tests.

--- methFinder.py
+++ methFinder.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from Bio import SeqIO
 
 
@@ -18,38 +19,69 @@ find the number and locations of recognitions sites in a given DNA template sequ
             - sequence_contexts (list): A list of sequence contexts surrounding the search string.
             
     """
+
+    if not os.path.exists(fasta_file):
+        raise FileNotFoundError("The input file was not found.")
+    
+
     results = []
 
-    try:
-        for fasta in SeqIO.parse(fasta_file, "fasta"):
-            name, sequence = fasta.id, str(fasta.seq.upper())
-            print(f"Processing sequence: {name}")
+    
+    for fasta in SeqIO.parse(fasta_file, "fasta"):
+        search_string = search_string.upper()
+        name, sequence = fasta.id, str(fasta.seq.upper())
+        print(f"Processing sequence: {name}")
 
-            # count the number of occurrences found of the search string.
-            num_occurrences = sequence.count(search_string)
-            positions = []
-            sequence_contexts = []
+        # count the number of occurrences found of the search string.
+        num_occurrences = sequence.count(search_string)
+        positions = []
+        sequence_contexts = []
 
-            # find all positions of the search string
-            start = 0
-            while start < len(sequence):
-                start = sequence.find(search_string, start)
-                if start == -1:
-                    break
-                positions.append(start)
-                
-                # Handle the case where the pattern is at the beginning of the sequence:
-                if start-num_bases < 0:
-                    sequence_contexts.append(sequence[0:start+len(search_string)+num_bases])
-                else:
-                    sequence_contexts.append(sequence[start-num_bases:start+len(search_string)+num_bases])
-                start += len(search_string)   # move start index to the end of the found substring.
+        # find all positions of the search string
+        start = 0
+        while start < len(sequence):
+            start = sequence.find(search_string, start)
+            if start == -1:
+                break
+            positions.append(start)
+            
+            # Handle the case where the pattern is at the beginning of the sequence:
+            if start-num_bases < 0:
+                sequence_contexts.append(sequence[0:start+len(search_string)+num_bases])
+            else:
+                sequence_contexts.append(sequence[start-num_bases:start+len(search_string)+num_bases])
+    
+            start += len(search_string)   # move start index to the end of the found substring.
+    
+        results.append((name, num_occurrences, positions, sequence_contexts))
         
-            results.append((name, num_occurrences, positions, sequence_contexts))
-        return results
-    except FileNotFoundError:
-        print("Error: The input file was not found.")
-        exit(1)
+    return results
+    
+
+def process_recognition_sites(input_file, search_string, num_bases, output_file_path=None):
+    results = find_methylase_recognition_sites(input_file, search_string.upper(), num_bases)
+
+    if output_file_path:
+        try: 
+            with open(output_file_path, "w") as output_file:
+                for name, num_occurrences, positions, sequence_contexts in results:
+                    output_file.write(f"name: {name}\n")
+                    output_file.write(f"search string: {args.find.upper()}\n")
+                    output_file.write(f"number of times found: {num_occurrences}\n")
+                    output_file.write(f"positions of {args.find}: {positions}\n")
+                    output_file.write(f"sequence contexts found: {sequence_contexts}\n\n")
+        except OSError as e:
+            print(f"Error: Unable to create or write to the output file '{output_file_path}'. Error: {e}")
+            exit(1)
+    else:
+        for name, num_occurrences, positions, sequence_contexts in results:
+            print(f"name: {name}")
+            print(f"search string: {args.find.upper()}")
+            print(f"number of times found: {num_occurrences}")
+            print(f"positions of {args.find}: {positions}")
+            print(f"sequence contexts found: {sequence_contexts}")
+            print()
+
 
 
 # Run main function:
@@ -63,29 +95,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Call the function to find the sequence contexts:
-    results = find_methylase_recognition_sites(args.input, args.find.upper(), args.numbases)
+    process_recognition_sites(args.input, args.find, args.numbases, args.output)
 
-    # Print or write the results to a file:
-    if args.output:
-        try:
-            with open(args.output, "w") as output_file:
-                for name, num_occurrences, positions, sequence_contexts in results:
-                    output_file.write(f"name: {name}\n")
-                    output_file.write(f"search string: {args.find}\n")
-                    output_file.write(f"number of times found: {num_occurrences}\n")
-                    output_file.write(f"positions of {args.find}: {positions}\n")
-                    output_file.write(f"sequence contexts found: {sequence_contexts}\n\n")
-        except OSError as e:
-            print(f"Error: Unable to create or write to the output file '{args.output}'. Error: {e}")
-            exit(1)
-    else:
-        for name, num_occurrences, positions, sequence_contexts in results:
-            print(f"name: {name}")
-            print(f"search string: {args.find}")
-            print(f"number of times found: {num_occurrences}")
-            print(f"positions of {args.find}: {positions}")
-            print(f"sequence contexts found: {sequence_contexts}")
-            print()
 
 
 
--- methfinder_unittests.py
+++ methfinder_unittests.py
@@ -1,25 +1,137 @@
+import os
 import unittest
+import tempfile
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+
+from unittest.mock import mock_open, patch
 from io import StringIO
-import sys
-from methFinder import main
+from methFinder import find_methylase_recognition_sites
+from methFinder import process_recognition_sites
 
 class TestScriptFunctionality(unittest.TestCase):
-    def setUp(self):
-        # Save the original stdout to restore it later
-        sys.original_stdout = sys.stdout
 
-    def tearDown(self):
-        # Restore the original stdout
-        sys.stdout = self.original_stdout
-
-    def test_pattern_found(self):
-        # Capture the output
-        captured_output = StringIO()
-        sys.stdout = captured_output
-
-        # Run the script with a FASTA file containing the pattern
-        main(['--input', 'fasta_files/dam_test.fasta', '--find', 'GATC', '--numbases', '2'])
-
-        # Assert the output contains the expected sequence contexts
-        self.assertIn('GCGATCAA', captured_output.getvalue())
     
+    # test finding methylase recognition sites in fasta file with multiple records.
+    def test_find_methylase_recognition_sites(self):
+        # Set up the FASTA file path
+        fasta_file = 'fasta_files/unittest.fasta'
+        
+        # Call the function with sample input:
+        results = find_methylase_recognition_sites(fasta_file, 'TCGA', 2)
+
+        # Verify the results:
+        self.assertEqual(len(results), 2)
+
+        # Check the first sequence:
+        name, num_occurrences, positions, sequence_contexts = results[0]
+        self.assertEqual(name, 'sequence1')
+        self.assertEqual(num_occurrences, 2)
+        self.assertEqual(positions, [1, 14])
+        self.assertEqual(sequence_contexts, ['ATCGATT', 'CATCGATG'])
+
+        # Check the second sequence
+        name, num_occurrences, positions, sequence_contexts = results[1]
+        self.assertEqual(name, 'sequence2')
+        self.assertEqual(num_occurrences, 2)
+        self.assertEqual(positions, [4, 17])
+        self.assertEqual(sequence_contexts, ['GATCGATT', 'CATCGATC']) 
+
+
+    # test the case where the pattern is at the beginning or end of the sequence.
+    def test_find_pattern_edges(self):
+        # Set up the FASTA file path
+        fasta_file = 'fasta_files/unittest_edges.fasta'
+    
+        # Call the function with sample input:
+        results = find_methylase_recognition_sites(fasta_file, 'tcga', 2)
+
+        # Verify the results:
+        self.assertEqual(len(results), 2)
+
+        # Check the beginning sequence:
+        name, num_occurrences, positions, sequence_contexts = results[0]
+        # print(results)
+        self.assertEqual(name, 'unittest_beginning')
+        self.assertEqual(num_occurrences, 1)
+        self.assertEqual(positions, [0])
+        self.assertEqual(sequence_contexts, ['TCGAAG'])
+
+        # Check the end sequence:
+        name, num_occurrences, positions, sequence_contexts = results[1]
+        self.assertEqual(name, 'unittest_end')
+        self.assertEqual(num_occurrences, 1)
+        self.assertEqual(positions, [20])
+        self.assertEqual(sequence_contexts, ['CCTCGA'])
+
+
+    # test the case where the script handles the case where the pattern is not found:
+    def test_find_negative_ctrl(self):
+        fasta_file = 'fasta_files/negative_ctrl.fasta'
+        
+        results = find_methylase_recognition_sites(fasta_file, 'GATC', 2)
+
+        self.assertEqual(len(results), 1)
+        
+        name, num_occurrences, positions, sequence_contexts = results[0]
+        self.assertEqual(name, 'negative_control_test')
+        self.assertEqual(num_occurrences, 0)
+        self.assertEqual(positions, [])
+        self.assertEqual(sequence_contexts, [])
+
+
+    # test the case where the context size is larger than the sequence length.
+    def test_tiny_sequence_large_context(self):
+        fasta_file = 'fasta_files/tiny_seq.fasta'
+        results = find_methylase_recognition_sites(fasta_file, 'gactgact', 2)
+
+        self.assertEqual(len(results), 1)
+
+        name, num_occurrences, positions, sequence_contexts = results[0]
+        self.assertEqual(name, 'tiny_sequence')
+        self.assertEqual(num_occurrences, 0)
+        self.assertEqual(positions, [])
+        self.assertEqual(sequence_contexts, [])
+
+
+    # test the case where the FASTA files does not exist.
+    def test_nonexistent_fasta(self):
+        fasta_file = 'fasta_file/positive_ctrl.fasta'
+
+        with self.assertRaises(FileNotFoundError) as context:
+            find_methylase_recognition_sites(fasta_file, 'gatc', 2)
+
+        self.assertEqual(str(context.exception), "The input file was not found.")
+    
+
+    # test the case where the output file cannot be created.
+    @patch('methFinder.open', new_callable=mock_open)
+    def test_output_file_oserror(self, mock_file):
+
+        # simulate OSError when trying to open the file
+        mock_file.side_effect = OSError("Mocked OSError for testing")
+
+        # create a temporary dummy FASTA file
+        with tempfile.NamedTemporaryFile(delete=False, mode='w') as temp_fasta:
+            temp_fasta_name = temp_fasta.name
+            record = SeqRecord(Seq("ATGCATGCGATCGATCGATCGATCG"), id="Test", description="Test Sequence")
+            SeqIO.write(record, temp_fasta_name, "fasta")
+
+
+        # Run the function
+        with self.assertRaises(SystemExit) as cm:   # Expecting a SystemExit due to 'exit(1)'
+            process_recognition_sites(temp_fasta_name, 'GATC', 2, 'output.txt')
+
+        # Clean up the temporary file:
+        os.remove(temp_fasta_name)
+
+        # Check if the exception raised was due to an error message.
+        self.assertEqual(cm.exception.code, 1)
+
+        # Ensure OSError was raised and caught
+        mock_file.assert_called_once_with('output.txt', 'w')
+
+
+if __name__ == '__main__':
+    unittest.main()
